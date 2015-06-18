3e115e4bad33d598b1c3decdd66306f44cae5c5b
Andrea Cedraro <a.cedraro@gmail.com>
2015-06-19 00:13:29 +0200

    Improve readability

--- spec/plugin/bigfiles_spec.rb
+++ spec/plugin/bigfiles_spec.rb
@@ -1,22 +1,27 @@
 require 'spec_helper'
 
-describe 'big files' do
+describe 'disabling YCM on big files' do
+  def ycm_active?
+    vim.echo('&completefunc') == 'youcompleteme#Complete'
+  end
+
   let (:root) { File.expand_path( '../../..', __FILE__ ) }
 
   before :each do
     vim.command("cd #{root}")
+    # lower the threshold for small/big files so we can test it easily
     vim.command('let g:ycm_disable_for_files_larger_than_kb = 5')
   end
 
-
-  it 'works fine when the file is smaller than g:ycm_disable_for_files_larger_than_kb' do
+  specify 'YCM act normal when the file is small' do
+    # completion_request.py is smaller than 5kb
     vim.edit('python/ycm/client/completion_request.py')
-    expect(vim.echo('&completefunc')).to eq("youcompleteme#Complete")
+    expect(ycm_active?).to be true
   end
 
-  it 'disable YCM when the file is larger than g:ycm_disable_for_files_larger_than_kb' do
+  specify 'YCM is disabled when the file is big' do
+    # base_request.py is larger than 5kb
     vim.edit('python/ycm/client/base_request.py')
-    expect(vim.echo('&completefunc')).not_to eq("youcompleteme#Complete")
-    # assert for the error message
+    expect(ycm_active?).to be false
   end
 end
