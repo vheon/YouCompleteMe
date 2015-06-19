daaa90d7d6e95a01276f3ed5d38a285e55cd258e
Andrea Cedraro <a.cedraro@gmail.com>
2015-06-19 02:04:25 +0200

    Skinnier test setup

--- spec/plugin/bigfiles_spec.rb
+++ spec/plugin/bigfiles_spec.rb
@@ -1,21 +1,18 @@
 require 'spec_helper'
 
 describe 'disabling YCM on big files' do
-  let (:root) { File.expand_path( '../../..', __FILE__ ) }
-
   before :each do
-    vim.command("cd #{root}")
     # lower the threshold for small/big files so we can test it easily
-    vim.command('let g:ycm_disable_for_files_larger_than_kb = 5')
+    vim.command 'let g:ycm_disable_for_files_larger_than_kb = 5'
   end
 
   specify 'YCM act normal when the file is small' do
-    vim.edit(fixture('4kb_file.py'))
+    vim.edit_fixture '4kb_file.py'
     expect(ycm_active?).to be true
   end
 
   specify 'YCM is disabled when the file is big' do
-    vim.edit(fixture('8kb_file.py'))
+    vim.edit_fixture '8kb_file.py'
     expect(ycm_active?).to be false
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,22 @@
 require 'vimrunner'
 require 'vimrunner/rspec'
 
-Vimrunner::RSpec.configure do |config|
-  pluging_path = File.expand_path( '../..', __FILE__ )
 
+Vimrunner::RSpec.configure do |config|
   config.start_vim do
     vim = Vimrunner.start
     # we have to manually source the plugin/youcompleteme.vim script and call
     # the Enable function because we load the plugin after vim is started so
     # the plugin directories are already loaded and the VimEnter event is
     # already fired
-    vim.add_plugin( pluging_path, 'plugin/youcompleteme.vim' )
+    plugin_path = File.expand_path( '../..', __FILE__ )
+    vim.add_plugin( plugin_path, 'plugin/youcompleteme.vim' )
     vim.command( 'call youcompleteme#Enable()' )
+
+    def vim.edit_fixture( name )
+      edit( File.join( __dir__, 'fixtures', name ) )
+    end
+
     vim
   end
-end
-
-def fixture(name)
-  File.join( File.expand_path( '../..', __FILE__ ), 'fixtures', name )
 end
