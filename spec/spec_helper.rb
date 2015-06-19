6f4b04031681e4d8809e32c1a2a8777b0ee46fea
Andrea Cedraro <a.cedraro@gmail.com>
2015-06-19 17:00:32 +0200

    Prefer gvim??

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ PLUGIN_PATH = File.expand_path( '../..', __FILE__ )
 
 Vimrunner::RSpec.configure do |config|
   config.start_vim do
-    vim = Vimrunner.start
+    vim = Vimrunner.start_gvim
     # we have to manually source the plugin/youcompleteme.vim script and call
     # the Enable function because we load the plugin after vim is started so
     # the plugin directories are already loaded and the VimEnter event is
