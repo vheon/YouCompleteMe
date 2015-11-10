aebd7208b5f254b4e6ddf2f71769d785a596a59d
Andrea Cedraro <a.cedraro@gmail.com>
2015-11-10 23:29:23 +0100

    X

--- install-vim.sh
+++ install-vim.sh
@@ -3,6 +3,7 @@ set -e
 
 git clone --depth 1 https://github.com/vim/vim /tmp/vim
 cd /tmp/vim
-./configure --prefix="${HOME}/vim" --with-features=huge --enable-pythoninterp
+./configure --prefix="${HOME}/vim" --with-features=huge \
+  --enable-pythoninterp --enable-fail-if-missing
 make -j2
 make install
